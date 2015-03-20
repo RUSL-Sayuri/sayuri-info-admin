4bc0ed025326127b092faeb94f934c505db2bc74
Thejan Rajapakshe <coder.clix@gmail.com>
2015-03-20 12:05:31 +0530

    Restructured the Google Analytics Library

---
+++ application/config/ga_api.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$config['ga_api_applicationName']="test-Analytics";
+$config['ga_api_clientId'] = "666657146859-mlofb35d8n25gso9srnps3dosrdg1ad9.apps.googleusercontent.com";
+$config['ga_api_clientSecret'] = "ilUyJccyZqSU0ZHJla3uw3Rf";
+$config['ga_api_redirectUri']="http://localhost/sayuri-info-admin/index.php/welcome/googleAnalytics";
+$config['ga_api_developerKey']="AIzaSyB1qZ_BTiS8MiE2OwKZWi-XbN3eoDo9RZI";
