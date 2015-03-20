4b860e188f5f58fee045804a35a147c7d67247ae
Thejan Rajapakshe <coder.clix@gmail.com>
2015-03-20 13:39:53 +0530

    Restructured the Google Analytics Library

--- application/config/ga_api.php
+++ application/config/ga_api.php
@@ -10,3 +10,4 @@ $config['ga_api_clientId'] = "666657146859-mlofb35d8n25gso9srnps3dosrdg1ad9.apps
 $config['ga_api_clientSecret'] = "ilUyJccyZqSU0ZHJla3uw3Rf";
 $config['ga_api_redirectUri']="http://localhost/sayuri-info-admin/index.php/welcome/googleAnalytics";
 $config['ga_api_developerKey']="AIzaSyB1qZ_BTiS8MiE2OwKZWi-XbN3eoDo9RZI";
+$config['ga_api_profileId'] = "99650545";
